8aad6bcf738ab7e417c4efc84eb7d43c4a4a69b4
Axelisin <axelisin@gmail.com>
2025-01-16 23:58:10 +0200

    updated code

--- temp_exch_rate_api.py
+++ temp_exch_rate_api.py
@@ -9,12 +9,28 @@ response = requests.get(url)
 if response.status_code == 200:
     data = response.json()
 
-    conversion_rate = data["conversion_rates"]["BGN"]
-    value = float(input("Please enter the currency amount: "))
-    bul_leva = value * conversion_rate
+    print("*** EUR/Currency Converter ***")
+    print("BGN")
+    print("USD")
+    print("GBP")
+    print("JPY")
+    print("CAD")
+    currency = input("Enter a currency from the list above: ").upper()
+    while True:
+        if currency == "BGN" or currency == "USD" or currency == "GBP" or currency == "JPY" or currency == "CAD":
+            break
+        else:
+            print("The currency is invalid!!")
+            currency=input("Enter a currency from the list above: ").upper()
+
+    amount = float(input("Enter the currency amount: "))
+
+    conversion_rate = data["conversion_rates"][f"{currency}"]
+    converted_amount = amount * conversion_rate
     current_data = data["time_last_update_utc"]
+
     print(f"Last update: {current_data}")
-    print(f"{value:.3f} EUR are equal to {bul_leva:.3f} BGN.")
+    print(f"{amount:.3f} EUR are equal to {converted_amount:.3f} {currency}.")
 
 else:
     print("Error connecting to the web service...")
